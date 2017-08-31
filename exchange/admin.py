6e9bb47df82c39ee0880c267ba24aee2677464ed
Sinan ISLEKDEMIR <sinan.islekdemir@metglobal.com>
2017-08-31 09:24:03 +0300

    Import models

--- exchange/admin.py
+++ exchange/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Currency, ExchangeRate
+from .models import Currency, ExchangeRate
 
 admin.site.register(Currency)
 admin.site.register(ExchangeRate)
